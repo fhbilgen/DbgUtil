bc9a2a4a65a4b4d6be6bcbc9fd97523f5521e10f
Faik Hakan Bilgen <faikb@microsoft.com>
2024-03-04 10:29:33 +0300

    get the latests

---
+++ FindnonZero/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Usage:
+// 
+
+var fileName = args[0];
+const string zeros = "00000000";
+string[] splitStrings = { "  ", " " };
+foreach (string line in System.IO.File.ReadLines(fileName))
+{
+    var parts = line.Split( splitStrings, 6, StringSplitOptions.RemoveEmptyEntries);
+    if (!parts[1].Equals(zeros) || !parts[2].Equals(zeros) || !parts[3].Equals(zeros) || !parts[4].Equals(zeros))
+        Console.WriteLine(line);
+}
+
+System.Console.WriteLine("Hit to exit");
+System.Console.ReadLine();
